8db02d5b5f1dc365fda1820803ed660ecb319794
Shaima Haque <shaimahaque02@yahoo.com>
2022-05-15 21:54:22 +0530

    Bot can detect all problems and message the user

--- index.js
+++ index.js
@@ -29,33 +29,43 @@ app.message('knock knock', async ({ message, say }) => {
     console.log("here");
   });
 
-  app.event('message', async ({event, client, logger}) =>{
-    try{
-      model = await toxicity.load(threshold);
-	    console.log('Ready!');
-    }
-    catch(error){
-      console.log(error);
-    }
-  });
+  // app.event('hello', async () =>{
+  //   try{
+  //     model = await toxicity.load(threshold);
+	//     console.log('Ready!');
+  //   }
+  //   catch(error){
+  //     console.log(error);
+  //   }
+  // });
   app.event('message', async ({ event, client, logger, say}) => {
     var pred = [];
     try {
       const txt = event.text;
-        console.log("Ready!");
-        let predictions = await model.classify(txt);
-        predictions.forEach(prediction=>{
-          if(prediction.results[0].match){
-            pred.push(prediction.label);
+      toxicity.load(threshold).then(model=>{
+        model.classify(txt).then(predictions => {
+          console.log(predictions.label);
+          predictions.forEach(prediction=>{
+            if(prediction.results[0].match){
+              pred.push(prediction.label);
+            }
+            });
+            var text_display = "";
+            if(pred.length > 1){
+            for(let i=0; i<pred.length-1;i++){
+                text_display += pred[i].charAt(0).toUpperCase() + pred[i].slice(1) + " and ";
+            }
+            text_display += pred[pred.length-1].charAt(0).toUpperCase() + pred[pred.length-1].slice(1);
+          }else{
+            text_display = pred[0].charAt(0).toUpperCase() + pred[0].slice(1);
           }
-        });
-      console.log(pred);
-      const result = await client.chat.postMessage({
-        channel: event.channel,
-        text: `${pred[0].charAt(0).toUpperCase()+pred[0].slice(1)} was detected in message sent by <@${event.user}>! Please adhere to community guidelines`
-      });
-    }
-    catch (error) {
+            const result = client.chat.postMessage({
+              channel: event.channel,
+              text: `${text_display} was detected in message sent by <@${event.user}>! Please adhere to community guidelines`
+            });
+          });
+        }); 
+      }catch (error) {
       logger.error(error);
     }
   });
